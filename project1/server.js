04f6ac2039e01a586880a0e6a78dd3daf0ecca21
hayden <haydenmcameron@proton.me>
2025-06-01 23:38:36 -0400

    handle get requests for / and /hayden

--- project1/server.js
+++ project1/server.js
@@ -2,4 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = 3003
 
+app.get('/', (req,res) => {
+    console.log('Woohoo', req.method)
+    res.sendStatus(200)
+})
+
+app.get('/hayden', (req,res) => {
+    console.log('/hayden endpoint reached')
+    res.send("Welcome to Hayden's Site!")
+})
+
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
